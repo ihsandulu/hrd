e081b94c1126034366a89a2c68a960d76fbcf1ae
ihsandulu <ihsan.dulu@gmail.com>
2025-03-27 11:00:52 +0700

    uang pengganti makan lembur romadlon

--- app/Views/master/midentity_v.php
+++ app/Views/master/midentity_v.php
@@ -98,6 +98,13 @@
                                     </div>
                                 </div>
 
+                                <div class="form-group">
+                                    <label class="control-label col-sm-12" for="identity_uanggantimakan">Uang Pengganti Makan Lembur Romadlon</label>
+                                    <div class="col-offset-sm-2 col-sm-10">
+                                        <input type="number" autofocus class="form-control" id="identity_uanggantimakan" name="identity_uanggantimakan" placeholder="" value="<?= $identity_uanggantimakan; ?>">
+                                    </div>
+                                </div>
+
                                 <!-- <div class="form-group">
                                     <label class="control-label col-sm-2" for="identity_about">About:</label>
                                     <div class="col-sm-10">
